baf3ca97f5adafd5206fb5ab62ce69fcae2591f8
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-10-10 16:22:15 +0700

    commit

---
+++ app/src/main/java/com/example/doan_music/LoadImage/LoadImageTask.java
@@ -0,0 +1,34 @@
+package com.example.doan_music.LoadImage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class LoadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView imageView;
+
+    public LoadImageTask(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... urls) {
+        String url = urls[0];
+        Bitmap bitmap = null;
+        try {
+            InputStream input = new java.net.URL(url).openStream();
+            bitmap = BitmapFactory.decodeStream(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        imageView.setImageBitmap(result);
+    }
+}
